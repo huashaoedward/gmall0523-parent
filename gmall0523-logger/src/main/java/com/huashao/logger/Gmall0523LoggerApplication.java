d9be784d676deeaec2399727c75a92e68421a214
hua_glb <hua_glb@qq.com>
2021-09-12 00:46:29 +0300

    first

---
+++ gmall0523-logger/src/main/java/com/huashao/logger/Gmall0523LoggerApplication.java
@@ -0,0 +1,14 @@
+package com.huashao.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//SpringBoot的程序入口
+@SpringBootApplication
+public class Gmall0523LoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0523LoggerApplication.class, args);
+    }
+
+}
